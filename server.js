b082d9eb4c15fec5a22332171f591b3b3e8e47fd
Paweł Zawadzki <admin@machinacyfrowa.pl>
2025-09-08 13:28:11 +0200

    Refactor comments for clarity and consistency in server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+//biblioteka do obsługi plików
 const fs = require('fs');
+//biblioteka serwera http
 let http = require('http');
 
-
+//tworzymy serwer ww odpowiadający na zapytania z przeglądarki
 http.createServer(
+    //deklarujemy funkcje która definiuje zachowanie serwera
+    //po otrzymaniu żądania (request) i co odeśle (response)
     function (request, response) {
+        //odczytujemy plik (asynchronicznie)
         fs.readFile('index.html', 'utf-8', (error, data) => {
         if(error) {//jeżeli nastąpił jakiś błąd podczas czytania pliku
             console.error("Błąd podczas odczytu pliku: " + error);
             return;
         }
+        //wysyłamy nagłówek do przeglądarki
         response.writeHead(200, {'Content-Type': 'text/html'});
+        //wysyłamy zawartośc pliku
         response.write(data);
+        //zamykamy odpowiedz
         response.end();
         })
     }
-).listen(8000);
+).listen(8000); //nasłuchuj na porcie 8000
